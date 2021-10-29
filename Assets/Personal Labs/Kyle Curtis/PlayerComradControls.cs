3c61b76a4bc206999c23d0da9bfa071fefff8fde
Kyle Curtis <kyle.curtis25@gmail.com>
2021-10-28 21:18:28 -0600

    Comrad_duck reporting
    I created comrad_duck, a copy of a turret, but with a duck's mesh. You can place in the same way you place a turret, but with F. right now it won't shoot properly, and its placement duck isn't playing nicely

---
+++ Assets/Personal Labs/Kyle Curtis/PlayerComradControls.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComradControls : MonoBehaviour
+{
+    //Public
+    [SerializeField] public GameObject placeableTurret;
+    [SerializeField] public GameObject placementTurret;
+    [SerializeField] public float placementCooldown = 5.0f;
+    [SerializeField] public float placementPreviewCooldown = 0.7f;
+    [SerializeField] public float placementDistance = 2.0f;
+
+    public float turnSmoothTime = 0.1f;
+
+
+    //Private
+    private float cooldown = 0;
+    private float previewCooldown;
+    private bool isInPreview = false;
+    private GameObject placeholder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previewCooldown = placementPreviewCooldown;
+        placeholder = new GameObject();
+    }
+    private void Update()
+    {
+        //Input for placing a turret
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (cooldown <= 0)
+            {
+
+                if (!isInPreview)
+                {
+                    //Instantiate the placement turret
+                    placeholder = Instantiate<GameObject>(placementTurret, this.transform);
+                }
+                isInPreview = true;
+
+                //wait a minimum time before accepting inputs
+                if (previewCooldown <= 0)
+                {
+                    //delete the placement turret
+                    Destroy(placeholder);
+
+                    //instantiate the actual turret
+                    Vector3 newPosition = new Vector3(transform.position.x + (placementDistance * this.transform.forward.x), transform.position.y, transform.position.z + (placementDistance * this.transform.forward.z));
+                    Instantiate<GameObject>(placeableTurret, newPosition, this.transform.rotation);
+
+                    //set the cooldown for placing a turret
+                    cooldown = placementCooldown;
+                    previewCooldown = placementPreviewCooldown;
+                    isInPreview = false;
+                }
+
+            }
+
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //update placement cooldown
+        if (cooldown < 0)
+        {
+            cooldown = 0;
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+        }
+        //update preview cooldown
+        if (previewCooldown < 0)
+        {
+            previewCooldown = 0;
+        }
+        else if (isInPreview)
+        {
+            previewCooldown -= Time.deltaTime;
+        }
+
+    }
+}
