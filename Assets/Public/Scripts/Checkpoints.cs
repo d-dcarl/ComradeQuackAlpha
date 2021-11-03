b29d75a2721ce916cb27017fcb4601db9a3dd5d3
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2021-11-02 20:49:26 -0600

    Dialogue Box

--- Assets/Public/Scripts/Checkpoints.cs
+++ Assets/Public/Scripts/Checkpoints.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Checkpoints : MonoBehaviour
 {
     public GameObject player;
+    public bool trigger = false;
     void OnTriggerEnter(Collider other)
     {
             player.GetComponent<PlayerMovement>().respawnPosition.position = player.GetComponent<Transform>().position;
-            //other.gameObject.transform.position = ;
+            gameObject.GetComponent<DialogueInteractable>().TriggerDialogue();
+            Destroy(gameObject.GetComponent<BoxCollider>());
+            //Destroy(GetComponent<DialogueInteractable>());
+            
     }
 }
 
