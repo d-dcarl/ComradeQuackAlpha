73bc19035c720e10840a86e26d5c1537783e3c81
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2021-11-09 17:55:54 -0700

    Collect and score manager

--- Assets/Public/Scripts/Collect.cs
+++ Assets/Public/Scripts/Collect.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour
 {
+    public int coinValue = 1;
     private void OnTriggerEnter(Collider other)
     {
-
+        
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "Bear")
         {
+            ScoreManager.instance.ChangeScore(coinValue);
             Destroy(gameObject);
         }
     }
