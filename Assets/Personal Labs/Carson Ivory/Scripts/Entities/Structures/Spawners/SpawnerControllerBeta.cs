e4ef2469d8e8857a971e9de055b940a646000baa
ckivory <ckivory@gmail.com>
2021-11-19 00:19:27 -0700

    Created prefabs for beta test spawner structures.

--- Assets/Personal Labs/Carson Ivory/Scripts/Entities/Structures/Spawners/SpawnerControllerBeta.cs
+++ Assets/Personal Labs/Carson Ivory/Scripts/Entities/Structures/Spawners/SpawnerControllerBeta.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShedControllerBeta : StructureControllerBeta
+public class SpawnerControllerBeta : StructureControllerBeta
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Personal Labs/Carson Ivory/Scripts/Ponds/DuckPondControllerBeta.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckPondControllerBeta : PondControllerBeta
+{
+    public StructureControllerBeta structure;
+}
