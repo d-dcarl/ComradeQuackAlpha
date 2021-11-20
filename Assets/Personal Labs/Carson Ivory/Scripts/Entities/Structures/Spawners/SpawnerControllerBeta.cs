e65962b21c38d9626de94e1a84daeff893a1a2ea
ckivory <ckivory@gmail.com>
2021-11-19 17:23:17 -0700

    Refactored spawner code.

--- Assets/Personal Labs/Carson Ivory/Scripts/Entities/Structures/Spawners/SpawnerControllerBeta.cs
+++ Assets/Personal Labs/Carson Ivory/Scripts/Entities/Structures/Spawners/SpawnerControllerBeta.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class SpawnerControllerBeta : StructureControllerBeta
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject spawnPrefab;  // Eventually this could be a list
+    public int spawnCap;
+    public float spawnDelay;
+    protected float spawnTimer;
+    public float spawnRadius;
+    public float spawnHeight;
+
+    protected List<GameObject> spawned;
+
+    public override void Start()
     {
-        
+        base.Start();
+        spawnTimer = spawnDelay;
+        spawned = new List<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
+        CleanSpawnedList();
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f && spawned.Count < spawnCap)
+        {
+            Spawn();
+            spawnTimer = spawnDelay;
+        }
     }
+
+    public void CleanSpawnedList()
+    {
+        int i = 0;
+        while (i < spawned.Count)
+        {
+            if(spawned[i] == null)
+            {
+                spawned.RemoveAt(i);
+            } else
+            {
+                i++;
+            }
+        }
+    }
+
+    public void Spawn()
+    {
+        Debug.Log(gameObject.name + " is trying to spawn " + spawnPrefab.name);
+        Vector3 offset = Random.onUnitSphere;                       // Random direction
+        offset = new Vector3(offset.x, 0f, offset.z).normalized;    // Flatten and make the offset 1 unit long
+        Vector3 spawnPosition = transform.position + (offset * spawnRadius) + (Vector3.up * spawnHeight);       // Make sure they don't spawn in the ground
+        GameObject newObject = Instantiate(spawnPrefab, spawnPosition, transform.rotation);
+        spawned.Add(newObject);
+    } 
 }
