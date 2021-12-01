5bf7cd235f15fe4b7d51e1e840f89fe8b6ad71fe
ckivory <ckivory@gmail.com>
2021-11-30 22:51:12 -0700

    Gave placeable turrets ability to be transparent.

--- Assets/Public/Scripts/Beta/Entities/Structures/Turrets/PlaceableTurretControllerBeta.cs
+++ Assets/Public/Scripts/Beta/Entities/Structures/Turrets/PlaceableTurretControllerBeta.cs
@@ -12,5 +12,27 @@ public class PlaceableTurretControllerBeta : TurretControllerBeta
         base.Start();
         placed = false;
         manned = false;
+        SetTransparent();
+        SetOpaque();
+    }
+
+    public void SetTransparent()
+    {
+        foreach(Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.shader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
+            Color oldColor = r.material.color;
+            r.material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 0.5f);
+        }
+    }
+
+    public void SetOpaque()
+    {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.shader = Shader.Find("Standard");
+            // Color oldColor = r.material.color;
+            // r.material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 0.5f);
+        }
     }
 }
