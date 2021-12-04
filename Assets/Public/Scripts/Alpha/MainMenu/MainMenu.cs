1624a3c9a0a9627dcf3ed29c01cb9f146a001e28
James Fullmer <james.e.fullmer@gmail.com>
2021-12-03 22:43:34 -0700

    Added more menu stuff

--- Assets/Public/Scripts/Alpha/MainMenu/MainMenu.cs
+++ Assets/Public/Scripts/Alpha/MainMenu/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,12 +11,22 @@ public class MainMenu : MonoBehaviour
     public GameObject full;
     public GameObject creditsImage;
     public GameObject videoPlayer;
+    public AudioSource backgroundMusic;
 
     public void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
+
+    //private void Update()
+    //{
+    //    if (!videoPlayer.activeSelf)
+    //    {
+    //        Debug.Log("no mute");
+    //        backgroundMusic.mute = false;
+    //    }
+    //}
 
     public void PlayGame()
     {
@@ -36,12 +47,15 @@ public class MainMenu : MonoBehaviour
             small.SetActive(false);
             full.SetActive(true);
         }
+
+        
     }
 
     public void PlayCredits()
     {
         videoPlayer.SetActive(true);
         creditsImage.SetActive(true);
+        //backgroundMusic.mute = true;
     }
 
     public void ExitGame()
--- Assets/Public/Scripts/Beta/WaveManagerBeta.cs
+++ Assets/Public/Scripts/Beta/WaveManagerBeta.cs
@@ -133,7 +133,7 @@ public class WaveManagerBeta : MonoBehaviour
         {
             Debug.Log("YOU WIN!");
             Cursor.lockState = CursorLockMode.None;
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("Level Clear");
             
         }
     }
