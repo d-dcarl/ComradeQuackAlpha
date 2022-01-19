30299f0c41b58d2a6ddc2509fbb784924a455b25
James Fullmer <james.e.fullmer@gmail.com>
2022-01-18 17:25:54 -0700

    Created sponsor pig

---
+++ Assets/Public/Scripts/Beta/Entities/Characters/AI/Enemies/SponsorPig.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SponsorPig : EnemyControllerBeta
+{
+    public HitboxControllerBeta effectsHitBox;
+    public float healthPerSecond;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+        HealAlly();
+    }
+
+    void HealAlly()
+    {
+        if (effectsHitBox.tracked.Count > 0)
+        {
+            foreach(GameObject g in effectsHitBox.tracked)
+            {
+                if (g.GetComponent<EnemyControllerBeta>())
+                {
+                    g.GetComponent<EntityControllerBeta>().Heal(healthPerSecond * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
