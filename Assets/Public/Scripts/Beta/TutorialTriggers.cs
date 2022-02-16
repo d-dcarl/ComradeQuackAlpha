a79fb642ca740b481ada80ed527500933c5dc129
jefullmer <james.e.fullmer@gmail.com>
2022-02-15 18:23:21 -0700

    Started working on tutorial logic

---
+++ Assets/Public/Scripts/Beta/TutorialTriggers.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTriggers : MonoBehaviour
+{
+    public string ID;
+    public bool keepPlayer;//Used to keep the player in place for dialogue parts
+
+    private void Update()
+    {
+        if (keepPlayer)
+        {
+            GameObject.Find("Player Beta").transform.position = transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<TutorialManager>().ReceiveTrigger(ID);
+        }
+    }
+}
