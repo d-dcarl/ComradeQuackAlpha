34408e917f9dcd1297302b9fb8ead609db30ed5c
Davis Canby <daviscanby@gmail.com>
2022-02-18 03:39:40 -0700

    Working on flying turret upgrades

---
+++ Assets/Public/Prefabs/Beta/Structures/Turrets/TowerUpgrades/FlyingTurretUpgrade.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tower Upgrades/Flying Turret Upgrade")]
+public class FlyingTurretUpgrade : TowerUpgrade
+{
+
+}
