7f00abff9b5427361d750f40ff38e9965146b0b3
Kyle Curtis <kyle.curtis25@gmail.com>
2022-02-27 18:15:17 -0700

    pigs jump
    after a random interval between 2 and 10 seconds pigs will perform a small hop

--- Assets/Public/Scripts/Beta/Entities/Characters/AI/Enemies/PigControllerBeta.cs
+++ Assets/Public/Scripts/Beta/Entities/Characters/AI/Enemies/PigControllerBeta.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class PigControllerBeta : EnemyControllerBeta
 {
+    [SerializeField] public bool isGrounded;
+    //was 30f, reduced to 10f
+    public float jumpSpeed = 10f;
+    protected float jumpTimer;
+    public float lowerJumpDelay = 2f;
+    public float upperJumpDelay = 10f;
 
+    public override void Start()
+    {
+        base.Start();
+        jumpTimer = Random.Range(lowerJumpDelay, upperJumpDelay);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if(!isGrounded)
+        {
+            if (jumpTimer <= 0)
+            {
+                pigJump();
+            }
+            else
+            {
+                jumpTimer -= Time.deltaTime;
+            }
+        }
+
+    }
+
+    public void pigJump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+        jumpTimer = Random.Range(lowerJumpDelay, upperJumpDelay);
+    }
 }
