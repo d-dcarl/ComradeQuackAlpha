99daa0619a3c6c65f6e2ca10e131f325c1380038
Kronoshifter <kronoshifter@gmail.com>
2022-04-07 13:59:35 -0600

    should fix build errors

--- Assets/Public/Scripts/Beta/Entities/Structures/Turrets/PlaceableTurretControllerBeta.cs
+++ Assets/Public/Scripts/Beta/Entities/Structures/Turrets/PlaceableTurretControllerBeta.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.TerrainAPI;
 using UnityEngine;
 
 public class PlaceableTurretControllerBeta : TurretControllerBeta
