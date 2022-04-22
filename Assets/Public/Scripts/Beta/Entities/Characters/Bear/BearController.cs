e4467a92040dabf9ced8b80d5eabdc839292cfc8
James Fullmer <james.e.fullmer@gmail.com>
2022-04-21 19:23:58 -0600

    Added bear animation

--- Assets/Public/Scripts/Beta/Entities/Characters/Bear/BearController.cs
+++ Assets/Public/Scripts/Beta/Entities/Characters/Bear/BearController.cs
@@ -9,13 +9,20 @@ public class BearController : MonoBehaviour
     public float speed;
     public float damageDelt;
     public float knockBack;
+    public float attackTimeMax;
 
     public bool go;
+
+    private Animator animator;
+    private float attackTimerCount;
+    private bool canStomp = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        attackTimerCount = attackTimeMax;
     }
 
     // Update is called once per frame
@@ -25,13 +32,43 @@ public class BearController : MonoBehaviour
         {
             transform.LookAt(targets[currentTargetIndex].transform);
             transform.position += transform.forward * speed * Time.deltaTime;
+            
+            if (!animator.GetBool("isWalking"))
+            {
+                animator.SetBool("isWalking", true);
+                animator.Play("Bear_Walk");
+                Debug.Log("walk");
+            }
+            attackTimerCount -= Time.deltaTime;
+            if (attackTimerCount <= 0)
+            {
+                canStomp = true;
+            }
+            
         }
+        //if (animator.GetCurrentAnimatorStateInfo(0).IsName("Bear Stomp") && !animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime)
+        //{
+        //    go = true;
+        //    animator.Play("Bear_Walk");
+        //    animator.SetBool("isAttacking", false);
+        //    Debug.Log("Stop stomp and go");
+        //}
     }
 
     public void Attack(GameObject enemy)
     {
         enemy.GetComponent<EnemyControllerBeta>().TakeDamage(damageDelt);
         enemy.GetComponent<Rigidbody>().AddForce(transform.forward * knockBack);
+        if (canStomp)
+        {
+            canStomp = false;
+            attackTimerCount = attackTimeMax;
+            go = false;
+            animator.SetBool("isAttacking", true);
+            animator.Play("Bear Stomp");
+            Debug.Log("Stop moving and stomp");
+            StartCoroutine(BackToWalk());
+        }
     }
 
     public void ReachNode()
@@ -43,6 +80,15 @@ public class BearController : MonoBehaviour
     {
         go = false;
         gameObject.SetActive(false);
+    }
+
+    IEnumerator BackToWalk()
+    {
+        yield return new WaitForSeconds(3f);
+        go = true;
+        animator.Play("Bear_Walk");
+        animator.SetBool("isAttacking", false);
+        Debug.Log("Stop stomp and go");
     }
 
 }
